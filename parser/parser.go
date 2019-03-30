8215de0d91c2232c81fedba79eaba694479f20ef
Zhenjun Wang <zjwang0414@gmail.com>
2019-03-31 00:04:55 +0800

    init parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/angeldswang/go-interpreter/ast"
+	"github.com/angeldswang/go-interpreter/lexer"
+	"github.com/angeldswang/go-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens to set curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParserProgram() *ast.Program {
+	return nil
+}
